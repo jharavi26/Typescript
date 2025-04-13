a72479aa3eeff36a6c41c902933b4af5f6665ed6
Ravikumar Jha <jharavi153@gmail.com>
2025-04-13 18:26:36 +0530

    Add Object

--- Basic/main.js
+++ Basic/main.js
@@ -1,4 +1,38 @@
+var _a;
 function addNumber(num1, num2) {
     return num1 + num2;
 }
 console.log(addNumber(10, 21));
+var age = 21;
+var isboolean = true;
+var firstName = "Ravikumar";
+console.log("".concat(age, ", ").concat(firstName, " , ").concat(isboolean));
+var numbers = [1, 2, 3];
+var names = ["Ravi", "Sam"];
+var user = ["Ravi", 25]; //Tuple = fixed number of elements with known types.
+console.log("".concat(numbers, ", ").concat(names, " , ").concat(user));
+var Role;
+(function (Role) {
+    Role[Role["Admin"] = 0] = "Admin";
+    Role[Role["User"] = 1] = "User";
+    Role[Role["Guest"] = 2] = "Guest";
+})(Role || (Role = {})); //Enus useful for name constants
+var myRole = Role.Guest;
+console.log(myRole);
+var data = "Ravi"; //Disables type checking — avoid if possible.
+data = true;
+console.log(data);
+function logMessage() {
+    console.log("Usecase of Void");
+}
+logMessage();
+var emp = {
+    name: "Ravi",
+    skills: ["React", "Redux"],
+    address: {
+        city: "Mumbai",
+        zip: 400001
+    }
+};
+console.log((_a = emp === null || emp === void 0 ? void 0 : emp.address) === null || _a === void 0 ? void 0 : _a.city);
+console.log(emp);
--- Basic/main.ts
+++ Basic/main.ts
@@ -1,5 +1,61 @@
 function addNumber(num1 : number, num2 : number) : number {
   return num1+num2;
-
 }
 console.log(addNumber(10,21));
+
+//Basic three DataTypes 
+
+let age : number = 21;
+let isboolean : boolean = true;
+let firstName : string = "Ravikumar";
+
+console.log(`${age}, ${firstName} , ${isboolean}`)
+
+let numbers: number[] = [1, 2, 3];
+let names: string[] = ["Ravi", "Sam"];
+let user: [string, number] = ["Ravi", 25];              //Tuple = fixed number of elements with known types.
+
+console.log(`${numbers}, ${names} , ${user}`)
+
+enum Role { Admin, User, Guest }                //Enus useful for name constants
+
+let myRole: Role = Role.Guest;
+
+console.log(myRole);
+
+let data : any = "Ravi"       //Disables type checking — avoid if possible.
+data = true;
+
+console.log(data);
+
+function logMessage(): void {            //function does something but doesn’t return anything
+  console.log("Usecase of Void");
+}
+
+logMessage();
+
+
+//Object Implementation 
+type Address = {
+  city: string;
+  zip: number;
+};
+
+type Employee = {
+  name: string;
+  address: Address;
+  skills : string[];
+  Degree?: number[];        //Use ? to indicate that a property is optional.
+};
+
+const emp: Employee = {
+  name: "Ravi",
+  skills : ["React", "Redux"],
+  address: {
+    city: "Mumbai",
+    zip: 400001
+  }
+};
+
+console.log(emp?.address?.city);
+console.log(emp);
